7b94bccc9d771a94d4bdfd0e0b0a519cca5eb3ea
kgseng <56063521+kgseng@users.noreply.github.com>
2020-11-22 15:13:14 -0800

    Kenny Seng & Patrick Oh
    dictionary_parser,py gather_meals updated
    app.py recipe-search & lucky routes updated
    lucky.html updated
    
    Updates are from last sprints - unseen in main branch merge.

--- dictionary_parser.py
+++ dictionary_parser.py
@@ -1,6 +1,27 @@
 import os
 from json_handler import JSONHandler
 
+def gather_meals():
+    """
+    When on the randomizer page, will gather all meals in our database return dictionary list with all meals.
+    
+     :return: list of pairs containing key and Display Name for the meals found. i.e.:
+     [[donut, Donut],
+     [veggieOmelet, Veggie Omelet],
+     [huevoRanchero, Huevo Ranchero]]
+    """
+    path = os.path.join(os.path.dirname(__file__), 'resources', 'meals.json')
+    return_list = []
+
+    # grab JSON to read - returns a dict
+    json = JSONHandler(path)
+    json_dict = json.read_json()
+
+    for key, value in json_dict.items():
+          return_list.append([key, value["Display Name"]])
+
+    return return_list
+
 
 def gather_meals_by_category(category: str):
     """
